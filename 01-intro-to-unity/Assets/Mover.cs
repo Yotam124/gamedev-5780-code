8c8ca4fb4a770f0807731de689c315d932e4708e
Erel Segal-Halevi <erelsgl@gmail.com>
2020-01-28 14:02:33 +0200

    first steps

---
+++ 01-intro-to-unity/Assets/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Start");
+        transform.position = new Vector3(0, 0, 0); // origin of world
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update");
+        transform.position += new Vector3(1, 0, 0); // move 1 unit right
+    }
+}
---
+++ 01-intro-to-unity/Assets/README.md
@@ -0,0 +1,6 @@
+Spaceship illustration taken from here:
+https://search.creativecommons.org/photos/bde97779-522d-4dc8-8138-9e4d2edfdfea
+"sat3" by marhawkman is licensed under CC BY 2.0.
+
+
+
