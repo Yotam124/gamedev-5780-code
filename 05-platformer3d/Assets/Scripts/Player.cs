bb37c59b3fe0e285f211957538afce1d904c5230
Erel Segal-Halevi <erelsgl@gmail.com>
2020-02-09 16:31:57 +0200

    week 5 unity

---
+++ 05-platformer3d/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    CharacterController characterController;
+
+    [Tooltip("In meters per second")] [SerializeField] float speed = 6.0f;
+    [SerializeField] float jumpHeight = 8.0f;
+    [SerializeField] float gravity = 1.0f;
+    [SerializeField] float yVelocity = 0f;
+
+    void Start() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        Vector3 velocity = new Vector3(0, 0, 0);
+        velocity.x = Input.GetAxis("Horizontal") * speed;
+
+        if (characterController.isGrounded) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                yVelocity = jumpHeight;
+            }
+        } else {
+            yVelocity -= gravity;
+        }
+        velocity.y = yVelocity;
+        characterController.Move(velocity * Time.deltaTime); // in meters per frame
+    }
+}
