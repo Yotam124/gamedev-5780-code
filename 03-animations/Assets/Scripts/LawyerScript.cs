82af048aa816ee6577f5f3d6103f6d71b2fbdd85
maoz-grossman <maozg2008@gmail.com>
2020-03-01 21:59:23 +0200

    The Lawyer animation
    The animation example from the summary

---
+++ 03-animations/Assets/Scripts/LawyerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LawyerScript : MonoBehaviour
+{
+    private Animator _anim;
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+        _anim.SetFloat("Move", direction);
+    }
+}
