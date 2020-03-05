adbebbfd6905f1af9f6cb213ddf7ff696df417d3
maoz-grossman <maozg2008@gmail.com>
2020-03-05 14:02:26 +0200

    Lawyer movment

--- 03-animations/Assets/Scripts/LawyerScript.cs
+++ 03-animations/Assets/Scripts/LawyerScript.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class LawyerScript : MonoBehaviour
 {
     private Animator _anim;
+    private SpriteRenderer _sr;
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         float direction = Input.GetAxis("Horizontal");
-        _anim.SetFloat("Move", direction);
-    }
+        if (direction < 0)
+        {
+            _sr.flipX = true;
+        }
+        else if (direction > 0)
+        {
+            _sr.flipX = false;
+        }
+        bool movment = false;
+        if (Mathf.Abs(direction) == 0)
+        {
+            movment = false;
+        }
+        else
+        {
+            movment = true;
+        }
+        _anim.SetBool("movment", movment);
+    }
+
 }
