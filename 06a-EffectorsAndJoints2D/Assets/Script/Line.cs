2acd33a992de7839a6092abac61dcbbb6134c152
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-19 21:32:58 +0300

    edit effectors and joints

--- 06a-EffectorsAndJoints2D/Assets/Script/Line.cs
+++ 06a-EffectorsAndJoints2D/Assets/Script/Line.cs
@@ -1,32 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Line : MonoBehaviour
-{
+/**
+ * This component draws a line between two gameObjects. 
+ * The line moves when the gameObjects move.
+ */ 
+public class Line : MonoBehaviour {
+    [Tooltip("The 1st endpoint of the line")] [SerializeField] GameObject gameObject1;
+    [Tooltip("The 2nd endpoint of the line")] [SerializeField] GameObject gameObject2; 
 
-    public GameObject gameObject1;          // Reference to the first GameObject
-    public GameObject gameObject2;          // Reference to the second GameObject
-
-    private LineRenderer line;                           // Line Renderer
-
-    // Use this for initialization
-    void Start()
-    {
-        // Add a Line Renderer to the GameObject
+    private LineRenderer line;
+    void Start() {
+        // Initialize the Line Renderer:
         line = this.gameObject.AddComponent<LineRenderer>();
-        // Set the width of the Line Renderer
+
+        // Set the width of the Line Renderer:
         line.SetWidth(0.05F, 0.05F);
-        // Set the number of vertex fo the Line Renderer
+
+        // Set the number of vertex of the Line Renderer:
         line.SetVertexCount(2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Check if the GameObjects are not null
-        if (gameObject1 != null && gameObject2 != null)
-        {
-            // Update position of the two vertex of the Line Renderer
+    void Update() {
+        if (gameObject1 != null && gameObject2 != null) {
+            // Update position of the two vertices of the Line Renderer:
             line.SetPosition(0, gameObject1.transform.position);
             line.SetPosition(1, gameObject2.transform.position);
         }
