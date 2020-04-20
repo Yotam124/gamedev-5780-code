935100a86ed8bc041de80ea0528b30f26b142400
Erel Segal-Halevi <erelsgl@gmail.com>
2020-04-20 22:11:48 +0300

    Edit basketball games

--- 06b-PhysicsCSharpFunctions/Assets/Script/AngryBird.cs
+++ 06b-PhysicsCSharpFunctions/Assets/Script/AngryBird.cs
@@ -1,37 +1,29 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class AngryBird : MonoBehaviour
-{
-    private bool _isPressed = false;
-    private Rigidbody2D rb;
-    [SerializeField]
-    private Rigidbody2D hook;
-    public float releaseTime = .15f;
-    public float maxDragDistance = 2f;
+
+public class AngryBird: MonoBehaviour {
+    [SerializeField] Rigidbody2D hook;
+    [SerializeField] float releaseTime = .15f;
+    [SerializeField] float maxDragDistance = 2f;
+
     static public int HIGH_SCORE = 0;
     static public int SCORE_FROM_PREV_ROUND = 0;
-    public int score = 0;
-    private bool _isScore = false;
 
+    private bool isMousePressed = false;
+    private bool isScore = false;
+    private int score = 0;
 
-    private void Start()
-    {
+    private Rigidbody2D rb;
+
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
-        score = HIGH_SCORE;
+        score = HIGH_SCORE = PlayerPrefs.GetInt("BasketballScore");
     }
 
-
-
-    void Update()
-    {
-        //If we currantky pressing the mouse down 
-        if (_isPressed)
-        {
+    void Update() {
+        if (isMousePressed) {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
             if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
                 rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
             else
@@ -39,71 +31,46 @@ public class AngryBird : MonoBehaviour
         }
     }
 
-
-
-    private void OnMouseDown()
-    {
-        _isPressed = true;
+    private void OnMouseDown() {
+        isMousePressed = true;
         rb.isKinematic = true;
-        //mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        //mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
     }
 
-
-
-    private void OnMouseUp()
-    {
-        _isPressed = false;
+    private void OnMouseUp() {
+        isMousePressed = false;
         rb.isKinematic = false;
-        StartCoroutine(Release());
+        StartCoroutine(ReleaseBall());
     }
 
-    IEnumerator Release()
-    {
-        yield return new WaitForSeconds(releaseTime);
+    IEnumerator ReleaseBall() {
+        // Wait a short time, to let the physics engine operate the spring and give some initial speed to the ball.
+        yield return new WaitForSeconds(releaseTime); 
         GetComponent<SpringJoint2D>().enabled = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "boundery")
-        {
-            if (!_isScore)
-                AddScore(_isScore);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-
-        if (other.tag == "Score")
-        {
-            _isScore = true;
-            AddScore(_isScore);
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "boundery") {
+            if (!isScore)
+                SetScore(0);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // restart game
+        } else if (other.tag == "Score") {
+            if (!isScore) {
+                isScore = true;
+                SetScore(score + 1);
+            }
         }
     }
 
-    private void AddScore(bool Addscore)
-    {
-        if (Addscore)
-        {
-            score++;
-            PlayerPrefs.SetInt("ProspectorHighScore", score);
-            print("Score: " + score);
-        }
-        else
-        {
-            score = 0;
-            PlayerPrefs.SetInt("ProspectorHighScore", score);
-            print("Score: " + score);
-        }
-
+    private void SetScore(int newScore) {
+        score = newScore;
+        PlayerPrefs.SetInt("BasketballScore", score);
+        print("Score: " + score);
     }
 
-    void OnGUI()
-    {
+    void OnGUI() {
         GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("label"));
         fontSize.fontSize = 16;
         fontSize.normal.textColor = Color.black;
-        GUI.Label(new Rect(100, 0, 200, 50), "Score: " + score, fontSize);
+        GUI.Label(new Rect(70, 0, 150, 50), "Score: " + score, fontSize);
     }
-
-
 }
--- 06b-PhysicsCSharpFunctions/Assets/Script/BasketballBall.cs
+++ 06b-PhysicsCSharpFunctions/Assets/Script/BasketballBall.cs
@@ -3,104 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BasketballBall : MonoBehaviour
-{
+public class BasketballBall: MonoBehaviour {
+    [SerializeField] float forceToAdd = 1300f;
+    [SerializeField] float timeFromHittingTheFloorToRestart = 2f;
+
     private Rigidbody2D rb;
-    private float _startTime;
-    private float _endTime;
-    private Vector3 mOffset;
-    private float mZCoord;
-    private Vector3 mEnd;
-    private Vector3 mStart;
-    public float ForceAdd = 1300f;
+    private Vector3 endPosition;
+    private Vector3 startPosition;
     private int Score = 0;
     private SpringJoint2D sj;
-    public float _timeToWait = 2f;
 
-
-    private void Start()
-    {
+    private void Start()  {
         sj = GetComponent<SpringJoint2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void OnMouseDown()
-
-    {
+    void OnMouseDown() {
         sj.enabled = false;
-        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        // Store offset = gameobject world pos - mouse world pos
-        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-        _startTime = Time.time;
         rb.isKinematic = true;
-        mStart = GetMouseAsWorldPoint();
-
+        startPosition = GetMouseAsWorldPoint();
     }
 
-
-
-    private Vector3 GetMouseAsWorldPoint()
-
-    {
-
-        // Pixel coordinates of mouse (x,y)
-        Vector3 mousePoint = Input.mousePosition;
-        // z coordinate of game object on screen
-        mousePoint.z = mZCoord;
-        // Convert it to world points
-        return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
-
-
-
-    void OnMouseDrag()
-
-    {
-
-        //transform.position = GetMouseAsWorldPoint() + mOffset;
+    void OnMouseDrag() {
         transform.position = GetMouseAsWorldPoint();
     }
 
-
-    void OnMouseUp()
-    {
-        _endTime = Time.time;
+    void OnMouseUp() {
         rb.isKinematic = false;
-        //mEnd = GetMouseAsWorldPoint() + mOffset;
-        mEnd = GetMouseAsWorldPoint();
-        float timeTO = 1/(_endTime - _startTime);
-        Vector3 ForceVec = (mEnd - mStart).normalized;
-        rb.AddForce(ForceVec * ForceAdd);
+        endPosition = GetMouseAsWorldPoint();
+        Vector3 forceVector = (endPosition - startPosition).normalized;
+        rb.AddForce(forceVector * forceToAdd);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "boundery")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
+    private Vector3 GetMouseAsWorldPoint() {
+        Vector3 mousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePoint.z = transform.position.z;
+        return mousePoint;
+    }
 
-        if (other.tag == "Platform")
-        {
-            StartCoroutine(HitTheFlore());
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "boundery") {
+            RestartLevel();
+        } else if (other.tag == "Platform") {
+            StartCoroutine(HitTheFloor());
             print("Platform");
         }
     }
 
-
-    IEnumerator HitTheFlore()
-    {
-        yield return new WaitForSeconds(_timeToWait);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    IEnumerator HitTheFloor() {
+        yield return new WaitForSeconds(timeFromHittingTheFloorToRestart);
+        RestartLevel();
     }
 
-    //void OnGUI()
-    //{
-    //    GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("label"));
-    //    fontSize.fontSize = 16;
-    //    fontSize.normal.textColor = Color.black;
-    //    GUI.Label(new Rect(25, 0, 200, 50), "Is sleeping?: " + rb.IsSleeping(), fontSize);
-    //    GUI.Label(new Rect(25, 25, 200, 50), "Is kinematic?: " + rb.isKinematic, fontSize);
-    //    GUI.Label(new Rect(25, 75, 300, 50), "Velocity: (x=" + rb.velocity.x.ToString("F2") + " ,y=" + rb.velocity.y.ToString("F2") + " ,z=" + rb.velocity.z.ToString("F2") + ")", fontSize);
-    //}
+    private void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
