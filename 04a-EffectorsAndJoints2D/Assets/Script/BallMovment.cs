553bc115a77231cfea4446ce6f2dd2c5dc2cd47e
Erel Segal-Halevi <erelsgl@gmail.com>
2020-05-01 15:10:06 +0300

    convert projects to 2019.3; rename hinge joint scene

--- 04a-EffectorsAndJoints2D/Assets/Script/BallMovment.cs
+++ 04a-EffectorsAndJoints2D/Assets/Script/BallMovment.cs
@@ -1,33 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BallMovment : MonoBehaviour
-{
+/**
+ * This component lets the user move its rigid body (e.g. a ball) using the keyboard, by adding force to the body.
+ */
+public class BallMovment : MonoBehaviour {
+    [Tooltip("How much force to add in the direction of movement in each frame")]
+    [SerializeField] private float forcePerFrame;
+
     private Rigidbody rb;
-    [SerializeField]
-    private float _speed;
     private Vector3 _starting_point;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody>();
         _starting_point = gameObject.transform.position;
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        Vector3 movment = new Vector3(x, 0, z);
-        rb.AddForce(movment * _speed);
+        Vector3 movement_direction = new Vector3(x, 0, z);
+        rb.AddForce(movement_direction * forcePerFrame);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag=="Bound")
-        {
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag=="Bound") {
             gameObject.transform.position = _starting_point;
         }
     }
